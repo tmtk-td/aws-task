669f59b5da0096ab4ed94ab2ec09bc80d1ca9d15
tmtk <blindhurts.6001@gmail.com>
2020-08-06 19:33:46 +0900

    再修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'aws-task'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/tmtk-toda/aws-task'
