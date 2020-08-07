772cca28ca5139d9381014f96f0d849ea8ed7e67
tmtk <blindhurts.6001@gmail.com>
2020-08-07 12:29:25 +0900

    init unicorn2

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/tmtk-toda/aws-task'
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/aws-task'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '18.176.40.206', user: 'app', roles: %w{app db web}
+server '13.113.113.202', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/tmtk/.ssh/id_rsa'
